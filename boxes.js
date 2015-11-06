8128ef43ff63a7f242dc6c9958798fa5863481f4
AriFlo <ariflo@arianflores.com>
2015-11-06 11:36:39 -0800

    removed CSS from boxes in first 2 rows

--- boxes.js
+++ boxes.js
@@ -1,5 +1,6 @@
 $( function (){
-//console.log( "DOM Ready for manipulation!" );
+console.log( "DOM Ready for manipulation!" );
+
 // Find the secretBox on the page. Set the background color to white. Add an h1 tag that says, "secret box!"
 $('#secretBox').css('background-color', 'white');
 $('<h1> secret box! </h1>').appendTo('#secretBox');
@@ -9,8 +10,14 @@ $('#row1').children().addClass('boxType3');
 
 // Make the last box in the last row disappear. (Hint, look into the dispaly style. Also, you should only get back one element from your selector.).
 $('#row4 div:last-child').remove();
+
 // Change all red boxes to white.
+$('.boxType1').css('background-color', 'white');
+
 // Get the first two divs in the second row. Take away all styling from the divs.
+$('#row2 div:lt(2)').removeClass();
+
+
 // Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
 
 
