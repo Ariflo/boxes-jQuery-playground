a375b0b6cf73625f1c72a422fee0ee9a6bcf1256
AriFlo <ariflo@arianflores.com>
2015-11-06 11:13:22 -0800

    added class to divs using jQuery

--- boxes.js
+++ boxes.js
@@ -1,11 +1,14 @@
 $( function (){
-    //console.log( "DOM Ready for manipulation!" );
+//console.log( "DOM Ready for manipulation!" );
 // Find the secretBox on the page. Set the background color to white. Add an h1 tag that says, "secret box!"
-
 $('#secretBox').css('background-color', 'white');
 $('<h1> secret box! </h1>').appendTo('#secretBox');
 
 // Find all child divs of the first row. Set the class for each div to boxType3.
+$('#row1').children().addClass('boxType3');
+$('.boxType3').css('background-color', 'black');
+
+
 // Make the last box in the last row disappear. (Hint, look into the dispaly style. Also, you should only get back one element from your selector.).
 // Change all red boxes to white.
 // Get the first two divs in the second row. Take away all styling from the divs.
