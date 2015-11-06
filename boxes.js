4e2d2a54527a66d232ff58d00ccac08cd9ff3456
AriFlo <ariflo@arianflores.com>
2015-11-06 14:05:44 -0800

    created excercise3 branch and added event handler

--- boxes.js
+++ boxes.js
@@ -1,26 +1,20 @@
 $( function (){
 
-	console.log( "DOM Ready for manipulation!" );
+	// Add an on click handler to the container div. Console log the x and y position of the click.
+	$('#container').on('click', function(e){
+	 	var cursorX = e.pageX;
+	    	var cursorY = e.pageY;
+	    	console.log(cursorX, cursorY);
+	} );
 
-
-
-	// Find the secretBox on the page. Set the background color to white. Add an h1 tag that says, "secret box!"
-	$('#secretBox').css('background-color', 'white');
-	$('<h1> secret box! </h1>').appendTo('#secretBox');
-
-	// Find all child divs of the first row. Set the class for each div to boxType3.
-	$('#row1').children().addClass('boxType3');
-
-	// Make the last box in the last row disappear. (Hint, look into the dispaly style. Also, you should only get back one element from your selector.).
-	$('#row4 div:last-child').remove();
-
-	// Change all red boxes to white.
-	$('.boxType1').css('background-color', 'white');
-
-	// Get the first two divs in the second row. Take away all styling from the divs.
-	$('#row2 div:lt(2)').removeClass();
-
-	// Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
-	$('.row').children('div:not(#secretBox)').css('width', '2px');	
-
+	// Add links inside all red box divs that take the user to galvanize. Then add an on click handler that alerts the user that you can never leave the page. Make sure the user won't leave the page after the alert!
+	// For all box divs, add a click handler that adds an image of a cute puppy to the box. If the image is clicked again, remove the cute puppy.
+	// Write a click handler on the conatainer div. The click handler should turn the container background to black and the background of the original div that was clicked to white. 
+	//If the user original div that was clicked happened to be the container div, change the background of the container div to lime green. You should not use any selectors for this exercise. 
+	//You can do it completely with what is given to you in the event callback.
 });
+
+
+
+
+
