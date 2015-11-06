4a1f2b30e0bd92d218351450efdf5a01ed4ceaae
AriFlo <ariflo@arianflores.com>
2015-11-06 11:24:22 -0800

    got rid of last box

--- boxes.js
+++ boxes.js
@@ -6,10 +6,9 @@ $('<h1> secret box! </h1>').appendTo('#secretBox');
 
 // Find all child divs of the first row. Set the class for each div to boxType3.
 $('#row1').children().addClass('boxType3');
-$('.boxType3').css('background-color', 'black');
-
 
 // Make the last box in the last row disappear. (Hint, look into the dispaly style. Also, you should only get back one element from your selector.).
+$('#row4 div:last-child').remove();
 // Change all red boxes to white.
 // Get the first two divs in the second row. Take away all styling from the divs.
 // Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
