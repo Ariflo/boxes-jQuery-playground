7395e94d11175e8923a22fd4883b0c3a2f160255
AriFlo <ariflo@arianflores.com>
2015-11-06 10:53:29 -0800

    notes secret box

--- boxes.js
+++ boxes.js
@@ -1,5 +1,20 @@
 $( function (){
-    console.log( "DOM Ready for manipulation!" );
+    //console.log( "DOM Ready for manipulation!" );
+// Find the secretBox on the page. Set the background color to white. Add an h1 tag that says, "secret box!"
+
+$('#secretBox').css('background-color', 'white');
+$('<h1> secret box! </h1>').appendTo('#secretBox');
+
+// Find all child divs of the first row. Set the class for each div to boxType3.
+// Make the last box in the last row disappear. (Hint, look into the dispaly style. Also, you should only get back one element from your selector.).
+// Change all red boxes to white.
+// Get the first two divs in the second row. Take away all styling from the divs.
+// Get all divs inside the container that are not row divs and are not the secret box div. Set the width of the divs to 2 pixels.
+
+
+
+
+
 });
 
 
